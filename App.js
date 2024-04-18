6822a8cdb7f374dd5ef3b9538c81b288ec783ba9
WvDeldenHR <wvdelden99@outlook.com>
2024-04-19 00:56:14 +0200

    fonts added

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, Text } from 'react-native';
+import { Text, SafeAreaView } from 'react-native';
+// Fonts
+import { useFonts, Raleway_400Regular, Raleway_500Medium, Raleway_600SemiBold, Raleway_700Bold, } from '@expo-google-fonts/raleway';
+import { Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, Poppins_700Bold, } from '@expo-google-fonts/poppins';
 
 export default function App() {
+
+    // Fonts
+    let [fontsLoaded] = useFonts({
+        // Raleway
+        Raleway_400Regular,
+        Raleway_500Medium,
+        Raleway_600SemiBold,
+        Raleway_700Bold,
+
+        // Poppins
+        Poppins_400Regular,
+        Poppins_500Medium,
+        Poppins_600SemiBold,
+        Poppins_700Bold,
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return (
         <SafeAreaView className="flex-[1] bg-secondary-light">
             <StatusBar style="auto" />
 
-            <Text className="">Open up App.js to start working on your app!</Text> 
+            <Text className="" style={{ fontFamily: 'Poppins_600SemiBold'}}>Open up App.js to start working on your app!</Text> 
         </SafeAreaView>
     );
 }
