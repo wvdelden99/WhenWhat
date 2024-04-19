f31d81f2e07bf3607a14680c2751bb8c8ae4c085
WvDeldenHR <wvdelden99@outlook.com>
2024-04-19 12:59:31 +0200

    home layout

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native';
 // Fonts
 import { useFonts, Raleway_400Regular, Raleway_500Medium, Raleway_600SemiBold, Raleway_700Bold, } from '@expo-google-fonts/raleway';
 import { Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, Poppins_700Bold, } from '@expo-google-fonts/poppins';
+// Views
+import { Home } from './views/Home';
 
 export default function App() {
 
@@ -29,7 +31,7 @@ export default function App() {
         <SafeAreaView className="flex-[1] bg-secondary-light">
             <StatusBar style="auto" />
 
-            <Text className="" style={{ fontFamily: 'Poppins_600SemiBold'}}>Open up App.js to start working on your app!</Text> 
+            <Home />
         </SafeAreaView>
     );
 }
---
+++ assets/styles/Styles.jsx
@@ -0,0 +1,3 @@
+export const colors = {
+    darkColor: "#212427",
+};
