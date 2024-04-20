3b24cb6c9dbd4918e1782674316d3eedc7bcde58
WvDeldenHR <wvdelden99@outlook.com>
2024-04-20 23:50:10 +0200

    layout update

--- views/Friends.jsx
+++ views/Friends.jsx
@@ -1,9 +1,9 @@
-import { Text, View } from 'react-native';
+import { Image, SafeAreaView, Text, View } from 'react-native';
 
 export function Friends() {
     return (
-        <View>
-            <Text>Friends</Text>
-        </View>
+        <SafeAreaView className="relative flex-[1] bg-secondary-light">
+            <Image className="absolute bg-cover" source={require('./../assets/static/images/image_background_01.png')}/>
+        </SafeAreaView>
     )
 }
