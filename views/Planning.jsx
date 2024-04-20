f1d7eb764490bfd6d1fb01daea09cbe84911377e
WvDeldenHR <wvdelden99@outlook.com>
2024-04-20 23:33:53 +0200

    planning localization

--- views/Planning.jsx
+++ views/Planning.jsx
@@ -1,14 +1,18 @@
+import { useTranslation } from 'react-i18next';
 import { Image, SafeAreaView, Text, TouchableHighlight, View } from 'react-native';
 import { color } from '../assets/styles/Styles';
 
 export function Planning() {
+    const { t } = useTranslation();
+
     return (
         <SafeAreaView className="bg-secondary-light">
-        <View className="">
+            <Image className="absolute bg-cover" source={require('./../assets/static/images/image_background_01.png')}/>
+
             <View className="px-6">
                 <View className="flex-row items-center my-6 px-3">
                     <View className="flex-[2]">
-                        <Text className="text-2xl text-white" style={{ fontFamily: 'Raleway_600SemiBold' }}>Plan something with your Friends</Text>
+                        <Text className="text-2xl text-white" style={{ fontFamily: 'Raleway_600SemiBold' }}>{t('planning.planning-header')}</Text>
                     </View>
 
                     <View className="flex-[1] items-center">
@@ -16,20 +20,18 @@ export function Planning() {
                     </View>
                 </View>
 
-                <View className="">
-                    <TouchableHighlight className="rounded-2xl py-4 bg-primary">
-                        <View className="flex-row justify-center items-center gap-2">
-                            <Image className="w-6 h-6" style={{ tintColor: color.darkColor }} source={require('./../assets/static/icons/icon_calendar_01.png')} />
-                            <Text className="text-xl text-dark" style={{ fontFamily: 'Raleway_700Bold' }}>Make Plans</Text>
-                        </View>
-                    </TouchableHighlight>
-                </View>
+                <TouchableHighlight className="rounded-2xl py-4 bg-primary">
+                    <View className="flex-row justify-center items-center gap-2">
+                        <Image className="w-6 h-6" style={{ tintColor: color.darkColor }} source={require('./../assets/static/icons/icon_calendar_01.png')} />
+                        <Text className="text-xl text-dark" style={{ fontFamily: 'Raleway_700Bold' }}>{t('planning.planning-button')}</Text>
+                    </View>
+                </TouchableHighlight>
             </View>
 
             <View className="mt-8 rounded-t-3xl px-6 h-full bg-white">
                 <View className="flex-row justify-between items-center my-4">
                     <View className="">
-                        <Text className="text-xl text-dark" style={{ fontFamily: 'Raleway_600SemiBold' }}>Planned</Text>
+                        <Text className="text-xl text-dark" style={{ fontFamily: 'Raleway_600SemiBold' }}>{t('planning.planning-subheader')}</Text>
                     </View>
                     
                     <View className="flex-row gap-1">
@@ -80,7 +82,6 @@ export function Planning() {
                     </View>
                 </View>
             </View>
-        </View>
         </SafeAreaView>
     )
 }
