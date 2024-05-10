c83cb06e661b7c152177185c189075a6f09e8c62
WvDeldenHR <wvdelden99@outlook.com>
2024-05-10 11:45:58 +0200

    language and font fix

--- App.js
+++ App.js
@@ -1,9 +1,32 @@
+import i18next from './localization/i18next';
+// Fonts
+import { useFonts, Raleway_400Regular, Raleway_500Medium, Raleway_600SemiBold, Raleway_700Bold, } from '@expo-google-fonts/raleway';
+// import { Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, Poppins_700Bold, } from '@expo-google-fonts/poppins';
 // Components
 import { AuthContextProvider } from "./config/auth/authContext";
 import { Navigation } from "./components/navigation/Navigation";
 
 
 export default function App() {
+    // Fonts
+    let [fontsLoaded] = useFonts({
+        // Raleway
+        Raleway_400Regular,
+        Raleway_500Medium,
+        Raleway_600SemiBold,
+        Raleway_700Bold,
+
+        // Poppins
+        // Poppins_400Regular,
+        // Poppins_500Medium,
+        // Poppins_600SemiBold,
+        // Poppins_700Bold,
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return (
         <AuthContextProvider>
             <Navigation />
